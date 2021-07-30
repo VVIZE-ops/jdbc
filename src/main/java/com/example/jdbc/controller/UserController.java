d64b98a897572ac1bbd7728e000150a6ca839f91
VVIZE-ops <zhulianwei213@Gmail.com>
2021-07-30 18:34:45 +0800

    新增

---
+++ src/main/java/com/example/jdbc/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.jdbc.controller;
+
+
+import com.example.jdbc.entity.User;
+import com.example.jdbc.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/insertUser")
+    public String insertUser(){
+        return "插入数据["+userService.insertUser()+"]条";
+    }
+
+    @RequestMapping("/insertGetKey")
+    public User insertGetKey(User user){
+        return userService.insertGetKey(user);
+    }
+
+    @RequestMapping("/selectByUsername")
+    public User selectByUsername(String username){
+        return userService.selectByUsername(username);
+    }
+
+    @RequestMapping("/findAll")
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+
+    @RequestMapping("/update")
+    public void update(User user){
+        userService.update(user);
+    }
+
+    @RequestMapping("/delete")
+    public void delete(Integer id){
+        userService.delete(id);
+    }
+}
