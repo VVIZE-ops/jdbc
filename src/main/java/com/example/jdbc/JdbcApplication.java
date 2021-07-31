9ad7c940a293edb36a1d05bc9692b8d0e9bc986d
VVIZE-ops <zhulianwei213@Gmail.com>
2021-07-31 09:28:41 +0800

    新增

--- src/main/java/com/example/jdbc/JdbcApplication.java
+++ src/main/java/com/example/jdbc/JdbcApplication.java
@@ -3,8 +3,12 @@ package com.example.jdbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = {"com.example"})
+
 public class JdbcApplication {
 
     public static void main(String[] args) {
