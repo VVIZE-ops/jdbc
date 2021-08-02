476ddf115f6531c38bea585cc0eb2b9eab977ea4
VVIZE-ops <zhulianwei213@Gmail.com>
2021-08-02 15:11:56 +0800

    测试

--- src/main/java/com/example/jdbc/JdbcApplication.java
+++ src/main/java/com/example/jdbc/JdbcApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
+//测试
 
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication(scanBasePackages = {"com.example"})
